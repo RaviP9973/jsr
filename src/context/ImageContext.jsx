8bc6cbb03220faddf24923078461fd552ba6ad45
RviP9973 <rp031776@gmail.com>
2024-12-08 23:57:53 +0530

    Done some styling

--- src/context/ImageContext.jsx
+++ src/context/ImageContext.jsx
@@ -5,9 +5,9 @@ export const ImageContext = createContext();
 export const ImageProvider = ({ children }) => {
   const [images, setImages] = useState([]);
   const [loading,setLoading] = useState(false);
-  const [files,setFiles] = useState(null);
+
   return (
-    <ImageContext.Provider value={{ images, setImages,loading,setLoading,files,setFiles }}>
+    <ImageContext.Provider value={{ images, setImages,loading,setLoading }}>
       {children}
     </ImageContext.Provider>
   );
